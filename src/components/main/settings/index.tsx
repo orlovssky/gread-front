55b4838d66f2e0983327c473789c8d355fbd2460
Sanzhar Alimkhanov <alimkhanovsanzhar@gmail.com>
2021-11-23 19:11:38 +0600

    css for theme

--- src/components/main/settings/index.tsx
+++ src/components/main/settings/index.tsx
@@ -22,24 +22,18 @@ const Settings = (): JSX.Element => {
         if (metaTag) {
           metaTag.setAttribute('content', '#1a1b1e');
         }
-        // document.documentElement.style.setProperty('background-color', '#1a1b1e');
-        // document.documentElement.setAttribute('data-theme', 'dark');
         setCurrentTheme('dark');
         break;
       case themes.light:
         if (metaTag) {
           metaTag.setAttribute('content', '#ffffff');
         }
-        // document.documentElement.style.setProperty('background-color', '#ffffff');
-        // document.documentElement.setAttribute('data-theme', 'light');
         setCurrentTheme('light');
         break;
       case themes.sepia:
         if (metaTag) {
           metaTag.setAttribute('content', '#f6f1ea');
         }
-        // document.documentElement.style.setProperty('background-color', '#f6f1ea');
-        // document.documentElement.setAttribute('data-theme', 'sepia');
         setCurrentTheme('sepia');
         break;
     }
